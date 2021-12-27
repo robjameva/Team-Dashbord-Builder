7c55ab7d72239b6a62c7a6d4c4ed96299c1156f3
Robert Evanik <rob.evanik@gmail.com>
2021-12-27 16:29:28 -0500

    Finished inquirer qurstions

--- index.js
+++ index.js
@@ -64,47 +64,7 @@ const initQuestions = [{
     default: 0
 }];
 
-
-// {
-//     type: 'input',
-//     name: 'github',
-//     message: 'Enter your GitHub Username (Required)',
-//     validate: githubInput => {
-//         if (githubInput) {
-//             return true;
-//         } else {
-//             console.log('Please enter your GitHub username!');
-//             return false;
-//         }
-//     }
-// },
-// {
-//     type: 'input',
-//     name: 'email',
-//     message: 'Enter your email address (Required)',
-//     validate: emailInput => {
-//         if (emailInput) {
-//             return true;
-//         } else {
-//             console.log('Please enter your email address!');
-//             return false;
-//         }
-//     }
-// },
-// {
-//     type: 'confirm',
-//     name: 'isOpenSource',
-//     message: 'Is this intended to be an open source project?',
-//     default: false
-// },
-// {
-//     type: 'input',
-//     name: 'contribution',
-//     message: 'Provide some guidelines for contribution to your project',
-//     when: ({ isOpenSource }) => isOpenSource
-// }
-
-const EngineerQuestions = [{
+const engineerQuestions = [{
     type: 'input',
     name: 'engineerName',
     message: "Plase enter the Engineer's Name (Required)",
@@ -169,6 +129,71 @@ const EngineerQuestions = [{
 }
 ];
 
+const internQuestions = [{
+    type: 'input',
+    name: 'internName',
+    message: "Plase enter the Intern's Name (Required)",
+    validate: internNameInput => {
+        if (internNameInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's Name!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internId',
+    message: "Plase enter the Intern's ID (Required)",
+    validate: internIdInput => {
+        if (internIdInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's ID!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internEmail',
+    message: "Plase enter the Intern's email address (Required)",
+    validate: internEmailInput => {
+        if (internEmailInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's email address!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internSchool',
+    message: "Plase enter the Intern's school (Required)",
+    validate: internSchoolInput => {
+        if (internSchoolInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's school!");
+            return false;
+        }
+    }
+},
+{
+    type: 'rawlist',
+    name: 'addEmployee',
+    message: "Choose another team member to add or select I'm Done",
+    choices: [
+        'Engineer',
+        'Intern',
+        "I'm Done!"
+    ],
+    default: 0
+}
+];
+
 
 const init = () => {
     return inquirer.prompt(initQuestions);
@@ -178,13 +203,33 @@ const promptEngineer = projectData => {
     if (!projectData.engineers) {
         projectData.engineers = [];
     }
-    return inquirer.prompt(EngineerQuestions)
+    return inquirer.prompt(engineerQuestions)
         .then(newEngineerData => {
             projectData.engineers.push(newEngineerData)
             if (newEngineerData.addEmployee === 'Engineer') {
-                promptEngineer
+                promptEngineer(projectData);
+            } else if (newEngineerData.addEmployee === 'Intern') {
+                promptIntern(projectData);
             } else {
-                console.log(projectData)
+                console.log(projectData);
+                return projectData;
+            }
+        })
+}
+
+const promptIntern = projectData => {
+    if (!projectData.interns) {
+        projectData.interns = [];
+    }
+    return inquirer.prompt(internQuestions)
+        .then(newInternData => {
+            projectData.interns.push(newInternData)
+            if (newInternData.addEmployee === 'Intern') {
+                promptIntern(projectData)
+            } else if (newInternData.addEmployee === 'Engineer') {
+                promptEngineer(projectData);
+            } else {
+                console.log(projectData);
                 return projectData;
             }
         })
@@ -192,8 +237,12 @@ const promptEngineer = projectData => {
 
 init()
     .then(data => {
-        console.log(data)
         if (data.addEmployee === "Engineer") {
             promptEngineer(data)
+        } else if (data.addEmployee === "Intern") {
+            promptIntern(data)
+        } else {
+            console.log(data)
+            return data
         }
     })
