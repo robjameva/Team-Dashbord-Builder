2f8bdd4a692944d9383df43c19dbe3e11c151b11
Robert Evanik <rob.evanik@gmail.com>
2021-12-27 16:38:23 -0500

    MOved questions to the src folder

---
+++ src/questions.js
@@ -0,0 +1,195 @@
+const initQuestions = [{
+    type: 'input',
+    name: 'managerName',
+    message: "Plase enter the Manager's Name (Required)",
+    validate: managerNameInput => {
+        if (managerNameInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Manager's Name!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'managerId',
+    message: "Plase enter the Manager's ID (Required)",
+    validate: managerIdInput => {
+        if (managerIdInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Manager's ID!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'managerEmail',
+    message: "Plase enter the Manager's email address (Required)",
+    validate: managerEmailInput => {
+        if (managerEmailInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Manager's email address!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'managerOffice',
+    message: "Plase enter the Manager's office number (Required)",
+    validate: managerOfficeInput => {
+        if (managerOfficeInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Manager's office number!");
+            return false;
+        }
+    }
+},
+{
+    type: 'rawlist',
+    name: 'addEmployee',
+    message: "Choose another team member to add or select I'm Done",
+    choices: [
+        'Engineer',
+        'Intern',
+        "I'm Done!"
+    ],
+    default: 0
+}];
+
+const engineerQuestions = [{
+    type: 'input',
+    name: 'engineerName',
+    message: "Plase enter the Engineer's Name (Required)",
+    validate: engineerNameInput => {
+        if (engineerNameInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Engineer's Name!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'engineerId',
+    message: "Plase enter the Engineer's ID (Required)",
+    validate: engineerIdInput => {
+        if (engineerIdInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Engineer's ID!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'engineerEmail',
+    message: "Plase enter the Engineer's email address (Required)",
+    validate: engineerEmailInput => {
+        if (engineerEmailInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Engineer's email address!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'engineerGithub',
+    message: "Plase enter the Engineer's github username (Required)",
+    validate: engineerGihubInput => {
+        if (engineerGihubInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Engineer's github username!");
+            return false;
+        }
+    }
+},
+{
+    type: 'rawlist',
+    name: 'addEmployee',
+    message: "Choose another team member to add or select I'm Done",
+    choices: [
+        'Engineer',
+        'Intern',
+        "I'm Done!"
+    ],
+    default: 0
+}
+];
+
+const internQuestions = [{
+    type: 'input',
+    name: 'internName',
+    message: "Plase enter the Intern's Name (Required)",
+    validate: internNameInput => {
+        if (internNameInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's Name!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internId',
+    message: "Plase enter the Intern's ID (Required)",
+    validate: internIdInput => {
+        if (internIdInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's ID!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internEmail',
+    message: "Plase enter the Intern's email address (Required)",
+    validate: internEmailInput => {
+        if (internEmailInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's email address!");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'internSchool',
+    message: "Plase enter the Intern's school (Required)",
+    validate: internSchoolInput => {
+        if (internSchoolInput) {
+            return true;
+        } else {
+            console.log("Plase enter the Intern's school!");
+            return false;
+        }
+    }
+},
+{
+    type: 'rawlist',
+    name: 'addEmployee',
+    message: "Choose another team member to add or select I'm Done",
+    choices: [
+        'Engineer',
+        'Intern',
+        "I'm Done!"
+    ],
+    default: 0
+}
+];
+
+module.exports = { initQuestions, engineerQuestions, internQuestions };
